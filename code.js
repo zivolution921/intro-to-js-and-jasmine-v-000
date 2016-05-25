c773d8a45ea19e6631c1455938d40a51306d7079
Ziv Zamechek <ziv921@yahoo.com>
2016-05-25 16:13:32 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(str) {
+  return "I love " + str;
+}
+
+function shouting(str) {
+  return str.toUpperCase();
+}
+
+function roundDown(dec) {
+  return Math.floor(dec);
+}
+
+function theTruth() {
+  return true;
+}
